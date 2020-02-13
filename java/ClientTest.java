a44da4ca437eb98deba41949650ce3feaa9ead5e
lance <3287596021@qq.com>
2020-02-13 10:35:34 +0800

    fix

--- java/ClientTest.java
+++ java/ClientTest.java
@@ -6,7 +6,6 @@ public class ClientTest {
 
     public static void main(String[] args) throws Exception{
 
-        //服务器端信息，address和8080；后台连接服务器，还会绑定客户端
         InetAddress address = InetAddress.getByName(null);
         System.out.println("address = " + address);
         for (int i = 0; i < 10; i++) {
